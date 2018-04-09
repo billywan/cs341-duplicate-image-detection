97ccde9c8b40064e0c551ed8d074e8307a9d256a
Billy Wan <billywxz@gmail.com>
2018-04-08 21:52:07 -0700

    added typo extensions

--- src/image_downloader.py
+++ src/image_downloader.py
@@ -29,7 +29,10 @@ links = data_frame['post_target_url'].tolist()
 links = list(filter(lambda l: isinstance(l, str), links))
 #print(list(links))
 
-IMAGE_FORMATS = ['png', 'jpg', 'gif', 'jpe', 'jpeg']
+IMAGE_FORMATS = ['png', 'jpg', 'gif']
+# grep '\.[a-z][a-z][a-z]$' imagelinks_submissions_2016_06.csv | grep -v jpg | grep -v png | grep -v gif | grep -v self,
+# grep '\.[a-z][a-z][a-z],$' imagelinks_submissions_2016_06.csv | grep -v jpg | grep -v png | grep -v gif | grep -v self, | grep -v imgur.com,
+TYPO_IMAGE_FORMATS = ['com', 'img', 'jgp', 'jph', 'jpd', 'jpp', 'jpq', 'jog', 'kpg', 'pbg', 'giv']
 # IMAGE_FORMATS = ['bmp', 'dib', 'eps', 'ps', 'gif', 'im', 'jpg', 'jpe', 'jpeg',
 #                  'pcd', 'pcx', 'png', 'pbm', 'pgm', 'ppm', 'psd', 'tif',
 #                  'tiff', 'xbm', 'xpm', 'rgb', 'rast', 'svg']
@@ -69,11 +72,11 @@ def download(links, dest_dir=None):
     imgur_links_count = len(imgur_links)
     image_links_count = len(image_links)
     print("total: {0}, imgur: {1}, image:{2}, combined valid: {3}".format(len(links), imgur_links_count, image_links_count, imgur_links_count+image_links_count))
-    
+
     # random.shuffle(imgur_links)
     # random.shuffle(image_links)
 
-    
+
     for link in imgur_links:
         img_name = "{0:010d}".format(count)
         try:
@@ -84,8 +87,8 @@ def download(links, dest_dir=None):
             raise
         except:
             print("Failed to download image at link", link)
-    
-    
+
+
     for link in image_links:
         img_name = "{0:010d}.png".format(count)
         try:
@@ -105,11 +108,3 @@ download(links, os.path.join(DATA_DIR, 'download'))
 # print(timeit.timeit('ImgurDownloader("http://i.imgur.com/H9GRMiT.jpg", "/Users/EricX/Desktop/CS341").save_images()', number=1, setup='from imgurdownloader import ImgurDownloader'))
 
 #urllib.request.urlretrieve("http://i.imgur.com/H9GRMiT.jpg", "00000001.jpg")
-
-
-
-
-
-
-
-
