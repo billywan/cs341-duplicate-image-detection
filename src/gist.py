3e9bb9faf01e111dc88de4d9bbbd27df0e5b0006
Billy Wan <billywxz@gmail.com>
2018-05-28 16:51:44 -0700

    compute gist vector for all queries

--- src/gist.py
+++ src/gist.py
@@ -16,6 +16,7 @@ def main():
     DATA_DIR = "/mnt/data/photoshopbattle_images_preprocessed"
     INPUT_DIR = os.path.join(PROJECT_DIR, '../data')
 
+    skipDirs = []
     if os.path.exists(os.path.join(INPUT_DIR, 'X.npy')):
         print "Input gist vectors already exist, skipping..."
     else:
@@ -24,17 +25,21 @@ def main():
         for dirName, _, fileList in os.walk(DATA_DIR):
             if len(fileList) > 1:
                 submission = sorted(fileList)[0]
-                fullPath = os.path.join(dirName, submission)
-                print "Computing gist vector for {}...".format(fullPath)
-                try:
-                    im = Image.open(fullPath)
-                    X.append(leargist.color_gist(im, orientations=(4,4,2)))
-                    submissionList.append(submission.rsplit('.', 1)[0])
-                    if len(submissionList) % 500 == 0:
-                        print "Computed gist vectors for {} images".format(len(submissionList))
-                        print "=" * 50
-                except:
-                    print "Unable to open image {}".format(fullPath)
+                # if no parent, skip
+                if submission.count("_") > 1:
+                    skipDirs.append(dirName)
+                else:
+                    fullPath = os.path.join(dirName, submission)
+                    print "Computing gist vector for {}...".format(fullPath)
+                    try:
+                        im = Image.open(fullPath)
+                        X.append(leargist.color_gist(im, orientations=(4,4,2)))
+                        submissionList.append(submission.rsplit('.', 1)[0])
+                        if len(submissionList) % 500 == 0:
+                            print "Computed gist vectors for {} images".format(len(submissionList))
+                            print "=" * 50
+                    except:
+                        print "Unable to open image {}".format(fullPath)
         X = np.array(X)
 
         print "Saving input gist vectors and submission list"
@@ -48,19 +53,18 @@ def main():
     Q = []
     queryList = []
     print "=" * 50
-    print "Randomly pick 100 query images..."
-    dirAndFiles = random.sample([(dirName, fileList) for dirName, _, fileList in os.walk(DATA_DIR) if len(fileList) > 1], 100)
-    print "Computing gist vectors for query images..."
-    for dir, files in dirAndFiles:
-        query = random.choice(sorted(files)[1:])
-        fullPath = os.path.join(dir, query)
-        print "Computing gist vector for query {}...".format(fullPath)
-        try:
-            im = Image.open(fullPath)
-            Q.append(leargist.color_gist(im, orientations=(4,4,2)))
-            queryList.append(query)
-        except:
-            print "Unable to open image {}".format(fullPath)
+    for dirName, _, fileList in os.walk(DATA_DIR):
+        if len(fileList) > 1 and dirName not in skipDirs:
+            print "Computing gist vectors for query images..."
+            for query in sorted(fileList)[1:]:
+                fullPath = os.path.join(dirName, query)
+                print "Computing gist vector for query {}...".format(fullPath)
+                try:
+                    im = Image.open(fullPath)
+                    Q.append(leargist.color_gist(im, orientations=(4,4,2)))
+                    queryList.append(query)
+                except:
+                    print "Unable to open image {}".format(fullPath)
     Q = np.array(Q)
 
     print "Saving query gist vectors and query list"
